ff7758c33944f49c050708c7f3dfaadd16b3537a
CodingClem <50382080+ClementGib@users.noreply.github.com>
2019-09-11 17:48:53 +0200

    Update install-debug.sh

--- install-debug.sh
+++ install-debug.sh
@@ -574,6 +574,7 @@ then
 		sudo apt-get update
 		sudo apt-get install -y kubelet kubeadm kubectl
 		sudo apt-mark hold kubelet kubeadm kubectl
+	fi
 fi
 
 ########################################################################
